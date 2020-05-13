1a1bc1ea53e62897a72cc0bfa727fba8045da370
susilnath <cycloners333@gmail.com>
2020-05-13 23:01:06 +0530

    changed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PyQt5 import QtWidgets, uic
 
 def fun():
     window.debug.setText("Working")
+
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
@@ -14,15 +15,8 @@ class Ui(QtWidgets.QMainWindow):
 
 if __name__ == '__main__':
     sched = BackgroundScheduler()
-
-    app = QtWidgets.QApplication(sys.argv)
     
-    window = uic.loadUi("main.ui")
-    window.Add_reminder.clicked.connect(fun)
-    window.show()
-    app.exec()
     window = Ui()
+    window.Add_reminder.clicked.connect(fun)
 
     # window.Add_reminder
-
-    app.exec()
