659ed9ff1440533998630ad5cc3eca05e3a772ce
Ragav <ragav@localhost.localdomain>
2020-05-14 00:00:53 +0530

    integrate apscheduler

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import sys
 import ui
+from datetime import datetime
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from PyQt5 import QtWidgets
+
+def Rem_fin(text):
+    print("hello")
+    window.label.setText(text)
+
+def Add_Rem():
+    sched.add_job(Rem_fin, trigger='date', run_date= datetime(2020,5,14,0,0,0) , args=['text'])
 
 if __name__ == '__main__':
     sched = BackgroundScheduler()
@@ -11,4 +19,9 @@ if __name__ == '__main__':
 
     window = ui.Ui()
     
+
+    sched.start()
+
+    Add_Rem()
+
     app.exec_()
