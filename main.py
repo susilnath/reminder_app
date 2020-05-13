52cdef816a68331c7c4c905280aefd4297ae6032
Ragav <ragav@localhost.localdomain>
2020-05-13 22:54:11 +0530

    proper initialisation of qt window

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 from PyQt5 import QtWidgets, uic
 
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('basic.ui', self)
+        self.show()
+
 if __name__ == '__main__':
     sched = BackgroundScheduler()
 
     app = QtWidgets.QApplication(sys.argv)
     
-    window = uic.loadUi("main.ui")
-    window.show()
+    window = Ui()
+
+    # window.Add_reminder
+
     app.exec()
