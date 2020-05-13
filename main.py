f3ece0c039a848b17778b276d0e7115535503962
susilnath <cycloners333@gmail.com>
2020-05-13 22:56:24 +0530

    added label

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 from PyQt5 import QtWidgets, uic
 
+def fun():
+    window.debug.setText("Working")
+
 if __name__ == '__main__':
     sched = BackgroundScheduler()
 
     app = QtWidgets.QApplication(sys.argv)
     
     window = uic.loadUi("main.ui")
+    window.Add_reminder.clicked.connect(fun)
     window.show()
     app.exec()
