d85bef41d93eefc8dfb7e24dddf563716b66bb3c
Ragav <ragav@localhost.localdomain>
2020-05-15 15:28:12 +0530

    added list functionality

--- main.py
+++ main.py
@@ -10,17 +10,23 @@ from PyQt5 import QtWidgets
 from ui import Ui
 
 # Reminder Functions
+
 def Rem_fin(text):
     MyUI.label.setText(text)
+    MyUI.Update_List(sched.get_jobs())
 
-def Add_Rem(date_time):
-    sched.add_job(Rem_fin, trigger='date', run_date= date_time , args=['finished'])
+def Add_Rem(Title, date_time):
+    sched.add_job(name= Title, trigger= 'date', run_date= date_time, func= Rem_fin, args= ['finished'])
+    
+    MyUI.Update_List(sched.get_jobs())
 
-# UI Handlers
-def ButtonPressed(self):
-    MyUI.label.setText(str(MyUI.datetime.dateTime().toString('yyyy-MM-dd hh:mm:ss')))
-    Add_Rem(str(datetime.fromisoformat(str(MyUI.datetime.dateTime().toString('yyyy-MM-dd hh:mm:ss')))))
+# UI Handlers1
+
+def ButtonPressed():
+    Add_Rem(Title= MyUI.RemTitle.text(), date_time= str(datetime.fromisoformat(str(MyUI.datetime.dateTime().toString('yyyy-MM-dd hh:mm:ss')))))
     # Add_Rem(dateutil.parser.isoparse(str(MyUI.datetime.dateTime().toString('yyyy-MM-dd hh:mm:ss'))))
+    
+    MyUI.label.setText(str(MyUI.datetime.dateTime().toString('yyyy-MM-dd hh:mm:ss')))   
 
 # Main Function
 if __name__ == '__main__':
@@ -28,11 +34,13 @@ if __name__ == '__main__':
     sched.add_jobstore('sqlalchemy',url= 'sqlite:///jobstore.db')
 
     app = QtWidgets.QApplication(sys.argv)          # Define QT app
-    
-    MyUI = Ui()                                     # Initialize UI
-    MyUI.button.clicked.connect(ButtonPressed)      # Connect Push Button to Handler
 
+    MyUI = Ui()                                     # Initialize UI
+    MyUI.button.clicked.connect(ButtonPressed)    # Connect Push Button to Handler
+    
     sched.start()
     sched.print_jobs()
-    
+
+    MyUI.Update_List(sched.get_jobs())
+
     app.exec_()
--- ui.py
+++ ui.py
@@ -2,20 +2,31 @@ from PyQt5 import QtWidgets, uic
 from PyQt5.QtCore import QDate,QTime
 
 class Ui(QtWidgets.QMainWindow):
+
+    
+
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('main.ui', self)
 
+        # self.joblist 
+
         self.button = self.findChild(QtWidgets.QPushButton, 'Add_reminder')
         self.label = self.findChild(QtWidgets.QLabel, 'debug')
-        self.datetime=self.findChild(QtWidgets.QDateTimeEdit,'DateTime_Picker')
-        self.remtext=self.findChild(QtWidgets.QDateTimeEdit,'rem_text')
+        self.datetime = self.findChild(QtWidgets.QDateTimeEdit, 'DateTime_Picker')
+        self.RemTitle = self.findChild(QtWidgets.QLineEdit, 'Reminder_Title')
+        self.joblist = self.findChild(QtWidgets.QListWidget, 'Job_List')
+
         self.datetime.setMinimumDate(QDate.currentDate())       #you can't bring back the dead
         self.datetime.setMinimumTime(QTime.currentTime())       #you can't bring back the dead :)
         self.datetime.setDisplayFormat('yyyy-MM-dd hh:mm:ss')
-        # self.button.clicked.connect(self.printButtonPressed)
 
         self.show()
 
-#     def ButtonPressed(self):
-#         self.label.setText(str(self.datetime.dateTime().toString()))
+    def Update_List(self, Item_list):
+
+        self.joblist.clear()
+
+        for item in Item_list:
+            self.joblist.addItem(item.name)
+
